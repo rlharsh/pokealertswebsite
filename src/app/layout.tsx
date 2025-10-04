0d4953f592e29d3ae3196e648f1e635fa3477d5f
Ronald Harsh <ronald.harsh@protonmail.com>
2025-10-03 23:40:41 -0500

    Updated Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -30,6 +31,21 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<head>
+				<Script
+					async
+					src="https://www.googletagmanager.com/gtag/js?id=G-ZJP8P69881"
+					strategy="afterInteractive"
+				/>
+				<Script id="google-analytics" strategy="afterInteractive">
+					{`
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', 'G-ZJP8P69881');
+					`}
+				</Script>
+			</head>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} antialiased`}
 			>
